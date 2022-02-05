b49b6313df3e7f86176c6a5e58d897b41cbc7515
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-02-04 18:15:46 -0800

    added pseudocode.js file and wrote beginning pseudocode for myself

---
+++ pseudocode.js
@@ -0,0 +1,62 @@
+// npm i 
+    // will need Mysql2, sequelize, dotenv
+// set db folder and schema.sql file
+// set .env file with necessary info
+// connect .env file with connection.js file in config folder
+// make sure connection.js file has all boiler plate information needed - compare to miniproject
+// 
+// check server.js file
+    // make sure all imports are there
+    // test server is listening
+    // make sure has necessary boiler plate code compared to miniproject
+    // sync sequelize models to the database and test server response
+// 
+// Build Models
+    // category.js model - refer to README
+    // product.js model - refer to README
+    // producttag.js model - refer to README
+    // tag.js model - refer to README
+    // make sure all are properly imported to the index.js file under models folder
+// 
+// Make sure seeds folder and seeds files are properly importing everything from Models folder
+// 
+// build and seed tables
+// mysql in terminal 
+    // select all on each table to make sure they are all there
+// 
+// build connections between all tables in index.js file in models folder
+// 
+// mysql in terminal 
+    // make sure tables are linking properly
+// 
+// Write Category routes 
+    // test in insomnia
+// 
+// Write product routes
+    // test in insomnia
+// 
+// Write tag routes
+    // test in insomnia
+// 
+// 
+// 
+// 
+// 
+// 
+// README completion
+// 
+// 
+// 
+// 
+// create walkthrough video 
+    // link on README
+    // submit walkthrough video link on bootcampspot
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
