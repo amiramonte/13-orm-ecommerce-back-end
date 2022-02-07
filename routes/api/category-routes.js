05b5630d93eeb9683e659232bc78ad17bf160617
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-02-07 15:45:33 -0800

    added get route for api/categories in category-route.js file, included associated products and tested route in postman, route successful

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,18 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [Product]
+    })
+    return res.status(200).json(categoryData)
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+
 });
 
 router.get('/:id', (req, res) => {
